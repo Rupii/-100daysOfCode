23d877f70c472eb8c9de5c906949b31e1d4232a7
rupii <mudavathrupesh@gmail.com>
2018-08-10 21:38:46 +0530

    day 1

---
+++ day1/expo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long x = 1e+3;
+
+    cout << x << "\n";
+    int y = 2;
+    for (int i = 31; i >= 0; i--)
+        (y | (1 << i)) ? cout << "1" : cout << "0";
+
+    int a = 3, b = 2;
+    int c = a / b;
+    cout << endl
+         << c;
+    return 0;
+}
